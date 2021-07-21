e695f7979cdfaee76a77028e07af2ee5e8716588
Justin4587 <Thurman@atlasok.com>
2021-07-21 10:53:18 -0500

    ad

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,4 +2,30 @@
 
 
 def canUnlockAll(boxes):
-    return True
+    """ numbers in lists """
+
+    copy_box = [False for l in range(len(boxes))]
+    copy_box[0] = True
+    keys = set(boxes[0])
+    new_keys = set()
+    total_boxes = len(boxes)
+    added = True
+
+    if (total_boxes == 0):
+        return True
+
+    while added:
+        added = False
+        keys = keys | new_keys
+        new_keys = set()
+
+        for i in keys:
+            if (i < total_boxes):
+                copy_box[i] = True
+                for j in boxes[i]:
+                    
+                    if j not in keys:
+                        new_keys.add(j)
+                        added = True
+                
+    return all(copy_box)
