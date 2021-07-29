b4b8554957b19bb65e33941e2c729e20cbfb8443
Justin4587 <Thurman@atlasok.com>
2021-07-29 16:40:10 -0500

    asdf

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+* insert_node - function for stuff
+* @head: asdfrg
+* @number: id
+* Return: line
+*/
+
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *list = NULL;
+    listint_t *copy = NULL;
+
+
+    list = malloc(sizeof(listint_t));
+    if (list ==NULL)
+        return (NULL);
+
+    list->n = number;
+
+    if (!*head || (*head)->n > number)
+    {
+        *head = list;
+        list->next = *head;
+    }
+    else
+    {
+        copy = *head;
+
+        while (copy->next && copy->next->n < number)
+            copy = copy->next;
+        list->next = copy->next;
+        copy->next = list; 
+    }
+    return(list);
+
+}
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,1 @@
+readme for 0x01 linked list or some variation of
