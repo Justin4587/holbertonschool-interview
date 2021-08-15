64a2c18ac5566df6007c9deb55b2b21e73b099d5
Justin4587 <Thurman@atlasok.com>
2021-08-15 15:17:47 -0500

    pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,12 +3,13 @@
 Main file for testing
 """
 
+
 def minOperations(n):
     min = 0
-    
+
     if n <= 1:
         return (0)
-    
+
     for i in range(2, (n + 1)):
         while (n % i == 0):
             min = min + i
