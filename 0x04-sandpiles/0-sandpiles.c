50480cc80ec543f4fd992bb3b095769315c889ec
Justin4587 <Thurman@atlasok.com>
2021-08-22 12:20:26 -0500

    does stuff

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -9,5 +9,31 @@
 */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    
+    int i, j;
+
+    for(i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            grid1[i][j] += grid2[i][j];
+
+    if((i = stable(grid1)) != 1)
+        printf("nope");
+    else
+        printf("ya");
+}
+
+/**
+* stable - 3 is the max
+*
+* @grid1: int
+* Return: 1 if stable
+*/
+int stable(int grid1[3][3])
+{
+    int i, j;
+
+    for(i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            if (grid1[i][j] <= 3)
+                return (1);
+    return (0);
 }
