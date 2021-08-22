f9b796b71caaa1964d02616fe9cd375d26511e33
Justin4587 <Thurman@atlasok.com>
2021-08-22 10:54:17 -0500

    hmm piles

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,13 @@
+#include "sandpiles.h"
+
+/**
+* sandpiles_sum - 3 is the max
+*
+* @grid1: int
+* @grid2: Pointer to the root of the tree to delete
+* Return: pointer to node
+*/
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    
+}
