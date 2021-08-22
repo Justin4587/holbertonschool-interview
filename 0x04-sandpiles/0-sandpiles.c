7f9306ef5c05e9c07784c99e580b82bdf3f8187a
Justin4587 <Thurman@atlasok.com>
2021-08-22 14:02:01 -0500

    more stuff

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -9,16 +9,16 @@
 */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+    int i;
 
-    for(i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
-            grid1[i][j] += grid2[i][j];
+    add_it(grid1, grid2);
 
-    if((i = stable(grid1)) != 1)
-        printf("nope");
-    else
-        printf("ya");
+    while((i = stable(grid1)) != 1)
+    {
+        print_grid(grid1);
+        do_stuff(grid1, grid2);
+    }
+
 }
 
 /**
@@ -36,4 +36,55 @@ int stable(int grid1[3][3])
             if (grid1[i][j] <= 3)
                 return (1);
     return (0);
+}
+
+/**
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
+ */
+static void print_grid(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/**
+ * add_it - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
+ */
+void add_it(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+    for(i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            grid1[i][j] += grid2[i][j];
+
+    for(i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            grid2[i][j] = 0;
+}
+
+/**
+ * do_stuff - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
+ */
+void do_stuff(int grid1[3][3], int grid2[3][3])
+{
+    print_grid(grid1);
+    print_grid(grid2);
+    printf("stuff");
 }
