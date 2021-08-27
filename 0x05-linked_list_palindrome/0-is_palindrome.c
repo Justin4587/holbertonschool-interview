7ab5bf27965330f6f4c93ed7cd4cc90cc1c9f77d
Justin4587 <Thurman@atlasok.com>
2021-08-27 16:09:53 -0500

    asdf

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - prints all elements of a listint_t list
+ * @head: pointer to head of list
+ * Return: 1 if palindrome, else 0
+ */
+ int is_palindrome(listint_t **head)
+ {
+     
+ }
