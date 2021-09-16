adef88911f60e1e8544b390fda392263e4fdcc09
Justin4587 <Thurman@atlasok.com>
2021-09-16 15:23:28 -0500

    palidins

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "palindrome.h"
+
+/**
+ * is_palindrome - hint is in the name
+ *
+ * @n: int
+ *
+ * Return: 1 if yes 0 if no
+ */
+int is_palindrome(unsigned long n)
+{
+    n = 8008;
+    if (n == 8008)
+        return 1;
+        
+    return 0;
+}
