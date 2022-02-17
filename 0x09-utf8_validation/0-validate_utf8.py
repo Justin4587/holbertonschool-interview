3ac49bfd4c981fb52fdd200279d5f197b311b751
Justin4587 <Thurman@atlasok.com>
2022-02-17 09:18:28 -0600

    trying another one

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,6 +4,7 @@ Main file for validating
 Lets see if the solution I found works
 """
 
+
 def validUTF8(data):
     """ should verify utf8 status """
     if not (type(data) is list and all([isinstance(n, int) for n in data])):
@@ -11,9 +12,8 @@ def validUTF8(data):
     data = [n + 256 if n < 0 and n > -129 else n for n in data]
     data = [n & 0xff if n >= 0 else n for n in data]
 
-    try: 
+    try:
         bytes(data).decode(encoding='utf-8', errors='strict')
     except (ValueError, UnicodeDecodeError):
         return False
     return True
- 
