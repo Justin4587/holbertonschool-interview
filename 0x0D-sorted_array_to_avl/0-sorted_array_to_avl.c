2d068c89acc6bde9e6865f2774e4c0e3899f077a
Justin Thurman <1828@holbertonschool.com>
2022-03-23 23:48:58 -0500

    last try fingers crossed

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -8,17 +8,17 @@
 
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    avl_t *head = NULL;
+	avl_t *head = NULL;
 
-    if (array == NULL || size == 0)
-        return (NULL);
+	if (array == NULL || size == 0)
+		return (NULL);
 
-    return (world_tree(head, array, 0, size - 1));
+	return (world_tree(head, array, 0, size - 1));
 }
 
 /**
  * world_tree - actual builder
- * @tree: head
+ * @head: head
  * @array: sorted array
  * @start: 0
  * @end: size - 1
@@ -27,27 +27,27 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 
 avl_t *world_tree(avl_t *head, int *array, size_t start, size_t end)
 {
-    size_t mid = (start + end) / 2;
-    avl_t *new;
+	size_t mid = (start + end) / 2;
+	avl_t *new;
 
-    if (start > end)
-        return (NULL);
+	if (start > end)
+		return (NULL);
 
-    new = malloc(sizeof(avl_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(avl_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->n = array[mid];
-    new->left = NULL;
-    new->right = NULL;
-    new->parent = head;
+	new->n = array[mid];
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = head;
 
-    if (new == NULL)
-        return (NULL);
-    if (mid != start)
-        new->left = world_tree(new, array, start, mid - 1);
-    if (mid != end)
-        new->right = world_tree(new, array, mid + 1, end);
+	if (new == NULL)
+		return (NULL);
+	if (mid != start)
+		new->left = world_tree(new, array, start, mid - 1);
+	if (mid != end)
+		new->right = world_tree(new, array, mid + 1, end);
 
-    return (new);
+	return (new);
 }
