31fa92f718a12f856631b6bd8fe5c34f75ed600c
Justin Thurman <1828@holbertonschool.com>
2022-04-12 18:14:00 -0500

    rain man

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""how much can it hold"""
+
+def rain(walls):
+	"""given integers determine amount"""
+	amount = 0
+	for i in range(len(walls)):
+		left = walls[i]
+		for j in range(i):
+			left = max(left, walls[j])
+		right = walls[i]
+		for j in range(i + 1, len(walls)):
+			right = max(right, walls[j])
+		amount += min(left, right) - walls[i]
+	return amount
