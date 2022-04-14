a21bb2793e8d43589f48c63e56fb7dfaa8003079
Justin Thurman <1828@holbertonschool.com>
2022-04-14 08:41:14 -0500

    peppy

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """how much can it hold"""
 
+
 def rain(walls):
-	"""given integers determine amount of water collected"""
-	amount = 0
-	for i in range(len(walls)):
-		left = walls[i]
-		for j in range(i):
-			left = max(left, walls[j])
-		right = walls[i]
-		for j in range(i + 1, len(walls)):
-			right = max(right, walls[j])
-		amount += min(left, right) - walls[i]
-	return amount
+    """given integers determine amount of water collected"""
+    amount = 0
+    for i in range(len(walls)):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        amount += min(left, right) - walls[i]
+    return amount
