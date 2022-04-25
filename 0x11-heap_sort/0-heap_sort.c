2a4bed7f1ec0a61e5b60be8532d0cb410433ee3f
Justin Thurman <1828@holbertonschool.com>
2022-04-24 21:25:13 -0500

    I need to start re-learning math

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -7,22 +7,22 @@
  */
 void heap_sort(int *array, size_t size)
 {
-  int i = 0;
-  int j;
-  int sizeint = (int)size;
+	int i = 0;
+	int j;
+	int sizeint = (int)size;
 
-  if (!array || size < 2)
-    return;
+	if (!array || size < 2)
+		return;
 
-  for (j = sizeint / 2; j > 0; j--)
-    heapify(array, size, j - 1, sizeint);
-    
-  for (i = sizeint - 1; i > 0; i--)
-    {
-      swap(&array[0], &array[i]);
-      print_array(array, size);
-      heapify(array, size, 0, i);
-    }
+	for (j = sizeint / 2; j > 0; j--)
+		heapify(array, size, j - 1, sizeint);
+
+	for (i = sizeint - 1; i > 0; i--)
+	{
+		swap(&array[0], &array[i]);
+		print_array(array, size);
+		heapify(array, size, 0, i);
+	}
 }
 
 /**
@@ -34,26 +34,26 @@ void heap_sort(int *array, size_t size)
  */
 void heapify(int *array, size_t size, int idx, int index)
 {
-  int largest = idx;
-  int left = idx * 2 + 1;
-  int right = idx * 2 + 2;
-  
-  if (left < index && array[left] > array[largest])
-  {
-    largest = left;
-  }
+	int largest = idx;
+	int left = idx * 2 + 1;
+	int right = idx * 2 + 2;
 
-  if (right < index && array[right] > array[largest])
-  {
-    largest = right;
-  }
+	if (left < index && array[left] > array[largest])
+	{
+		largest = left;
+	}
 
-  if (largest != idx)
-  {
-    swap(&array[idx], &array[largest]);
-    print_array(array, size);
-    heapify(array, size, largest, index);
-  }
+	if (right < index && array[right] > array[largest])
+	{
+		largest = right;
+	}
+
+	if (largest != idx)
+	{
+		swap(&array[idx], &array[largest]);
+		print_array(array, size);
+		heapify(array, size, largest, index);
+	}
 }
 
 /**
@@ -64,7 +64,7 @@ void heapify(int *array, size_t size, int idx, int index)
  */
 void swap(int *a, int *b)
 {
-  int swap = *a;
-  *a = *b;
-  *b = swap;
+	int swap = *a;
+	*a = *b;
+	*b = swap;
 }
