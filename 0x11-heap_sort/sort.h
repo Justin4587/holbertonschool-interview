a764538b7638d03eb33825472851d0dc2297bf29
Justin Thurman <1828@holbertonschool.com>
2022-04-24 21:28:56 -0500

    I need to start re-learning math

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -4,9 +4,9 @@
 #include <stddef.h>
 
 
-void print_array(const int* array, size_t size);
-void heap_sort(int* array, size_t size);
-void swap(int* a, int* b);
-void heapify(int* array, size_t size, int idx, int index);
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void swap(int *a, int *b);
+void heapify(int *array, size_t size, int idx, int index);
 
 #endif /* SORT_H */
