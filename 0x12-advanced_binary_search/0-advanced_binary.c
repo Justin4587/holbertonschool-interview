2cdd61ec5dec640cf262efb93c02b7e5b312b500
Justin Thurman <1828@holbertonschool.com>
2022-04-29 20:56:59 -0500

    I really hate losing points over stupid shit. I'm gussing my compiler was a different version or something

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -52,7 +52,7 @@ void print_it(int *array, int start, int sub)
 {
 	int i;
 	printf("Searching in array: ");
-	for ( i = start; i <= sub; i++)
+	for (i = start; i <= sub; i++)
 	{
 		printf("%d", array[i]);
 		if (i < sub)
