092c85cfa422b87ef1a84d4636584f0c1713fc92
Justin Thurman <1828@holbertonschool.com>
2022-05-08 12:00:52 -0500

    pep8 stuff

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -8,39 +8,39 @@ from requests import get
 
 
 def count_words(subreddit, word_list, dict={}, after='null'):
-  """ lets count some shit """
+    """ lets count some shit """
 
+    sub = subreddit
+    head = {'user-agent': 'test'}
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(sub, after)
+    dict = OrderedDict(dict)
+    word_list = [w.lower() for w in word_list]
 
-  head = { 'user-agent': 'test' }
-  url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit, after)
-  dict = OrderedDict(dict)
-  word_list = [w.lower() for w in word_list]
+    hot = get(url, headers=head)
+    if (hot.status_code != 200) or len(word_list) == 0:
+        return
 
-  hot = get(url, headers=head)
-  if (hot.status_code != 200) or len(word_list) == 0:
-    return
+    hotJson = hot.json()
+    data = hotJson.get('data')
+    chillins = data.get('children')
 
-  hotJson = hot.json()
-  data = hotJson.get('data')
-  chillins = data.get('children')
-
-  if (data and chillins):
-    for title in chillins:
-      titleLow = (title.get('data')['title']).lower().split()
-      for i in titleLow:
-        for w in word_list:
-          if i == w:
-            if w in dict.keys():
-              dict[w] += 1
-            else:
-              dict[w] = 1
+    if (data and chillins):
+        for title in chillins:
+            titleLow = (title.get('data')['title']).lower().split()
+            for i in titleLow:
+                for w in word_list:
+                    if i == w:
+                        if w in dict.keys():
+                            dict[w] += 1
+                        else:
+                            dict[w] = 1
 
     AFTER = hotJson.get("data").get("after")
     if (AFTER is not None):
-      return count_words(subreddit, word_list, dict, AFTER)
+        return count_words(subreddit, word_list, dict, AFTER)
     else:
-      if (word_list == []):
-        return None
-      else:
-        for i in sorted(dict, key=dict.get, reverse=True):
-          print('{}: {}'.format(i, dict[i]))
+        if (word_list == []):
+            return None
+        else:
+            for i in sorted(dict, key=dict.get, reverse=True):
+                print('{}: {}'.format(i, dict[i]))
