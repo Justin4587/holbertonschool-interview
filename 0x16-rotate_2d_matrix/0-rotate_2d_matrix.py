258f8c47b5594c8201c5a52987eb42bbe00cea15
Justin Thurman <1828@holbertonschool.com>
2022-05-25 11:06:59 -0500

    pep8 fix

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,16 +5,17 @@ Test 0x16 - Rotate 2D Matrix
 
 
 def rotate_2d_matrix(matrix):
-  """Rotate it 90 degrees"""
-  pass
-  N = len(matrix)
+    """Rotate it 90 degrees"""
+    N = len(matrix)
 
-  """nested loop to swap places"""
-  for i in range(int(N / 2)):
-    for j in range(i, int(N - i - 1)):
+    """nested loop to swap places"""
+    for i in range(int(N / 2)):
+        for j in range(i, int(N - i - 1)):
 
-      temp = matrix[i][j]
-      matrix[i][j] = matrix[N - 1 - j][i]
-      matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j]
-      matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
-      matrix[j][N - 1 - i] = temp
+            temp = matrix[i][j]
+            """matrix[i][j] is starting point.
+            using the len we find the corresponding spot and swap."""
+            matrix[i][j] = matrix[N - 1 - j][i]
+            matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j]
+            matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
+            matrix[j][N - 1 - i] = temp
