af43b3cb8e8028b4a8b8173dc07eeef3cc3bd8cc
Justin Thurman <1828@holbertonschool.com>
2022-09-12 20:52:42 -0500

    making triangle like shapes with numbers and such

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""pascal triangle generator"""
+
+def pascal_triangle(n):
+  """Generate a triangle"""
+
+  list = []
+
+  if n <= 0:
+    return list
+
+  for each_row in range(n):
+    row = [None for _ in range(each_row + 1)]
+    row[0] = 1
+    row[-1] = 1
+
+    for i in range(1, len(row) - 1):
+      row[i] = list[each_row - 1][i - 1] + list[each_row - 1][i]
+
+    list.append(row)
+
+  return list
---
+++ 0x1F-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+Triangle shananigans
